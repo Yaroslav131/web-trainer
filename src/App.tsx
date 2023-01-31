e0ec4895faf3d0f9d3bd5903d5186f22442ea944
Yaroslav131 <berserk12186@gmail.com>
2023-01-31 11:01:51 +0300

    Initialize project using Create React App

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import MainPage from './Components/MainPage/MainPage';
+import Exercise from './Classes/Execirse';
 
 function App() {
+
+  const excersises = [];
+
+  for (let index = 0; index <= 32; index++) {
+    if (index % 5 == 0) {
+      excersises.push(new Exercise(`${index}`, true))
+    }
+    else {
+      excersises.push(new Exercise(`${index}`, false))
+    }
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MainPage excersises={excersises} />
   );
 }
 
