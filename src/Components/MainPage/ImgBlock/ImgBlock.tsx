e8c1faf2925f7ad0b6f7e7f047e307f8c614bd26
Yaroslav131 <berserk12186@gmail.com>
2023-02-13 20:57:11 +0300

    Add img block

---
+++ src/Components/MainPage/ImgBlock/ImgBlock.tsx
@@ -0,0 +1,30 @@
+import React, { useEffect } from "react";
+import '../../../reset.css'
+import './ImgBlock.css'
+
+export default function ImgBlock(props: any) {
+
+    let img = "";
+    let body;
+    try {
+        img = require(`../../../imgs/${props.imgURL}`);
+    }
+    catch (e) {
+        console.log(e);
+        body = <> </>;
+    }
+
+    if (img != "") {
+        body = <div className="img-contaiter">
+            <div className="task-img">
+                <img src={img} alt="Задание" />
+            </div>
+        </div>
+    }
+
+    return (
+        <div>
+            {body}
+        </div>
+    )
+}
