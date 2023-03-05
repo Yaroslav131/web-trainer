6baec32deaf50b52083c733841300232f2222fcc
Yaroslav131 <berserk12186@gmail.com>
2023-03-05 16:24:15 +0300

    change header

--- src/Components/MainPage/Header/Header.tsx
+++ src/Components/MainPage/Header/Header.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import '../../../reset.css'
 import './Header.css'
 import home from '../../../imgs/home-page.png'
+import { Link } from "react-router-dom"
 
 export default function Header() {
     return (
-        <header>
-            <div  className="home-container">
-            <img className="home" src={home} alt="Меню" />
-            </div>
+        <header className="header">
+            <Link to="/"><div className="home-container">
+                <img className="home" src={home} alt="Меню" />
+            </div></Link>
             <div className="logo-container">
                 <p className="logo"> Web Trainer</p>
             </div>
