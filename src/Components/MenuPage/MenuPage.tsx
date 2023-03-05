23a5ae3abb504282b5097f728ed6bf96204b59f2
Yaroslav131 <berserk12186@gmail.com>
2023-03-05 16:23:37 +0300

    Add menu page

---
+++ src/Components/MenuPage/MenuPage.tsx
@@ -0,0 +1,24 @@
+import React, { useState } from "react";
+import {  Link } from "react-router-dom"
+import './MenuPage.css'
+import '../../reset.css'
+
+export default function MenuPage(props: any) {
+    return (
+        <body>
+            <header className="mainPage-header">
+                <h1>Web Trainer</h1>
+            </header>
+
+            <div className="buttons">
+                <div className="button__container">
+                <Link to="/exercises/html-exersises"><input type="submit" value="HTML Trainer" /></Link>
+                </div>
+                <div className="button__container">
+                <Link to="/exercises/css-exersises"><input type="submit" value="CSS Trainer" /></Link>
+                </div>
+            </div>
+
+        </body>
+    )
+}
