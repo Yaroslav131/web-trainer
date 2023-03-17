0bf721d0a51c36288ad066b2ac948adf7b9f339d
Yaroslav131 <berserk12186@gmail.com>
2023-03-17 10:34:09 +0300

    change header component

--- src/Components/MainPage/Header/Header.tsx
+++ src/Components/MainPage/Header/Header.tsx
@@ -2,9 +2,10 @@ import React from "react";
 import '../../../reset.css'
 import './Header.css'
 import home from '../../../imgs/home-page.png'
+import menu from '../../../imgs/humburger-menu.png'
 import { Link } from "react-router-dom"
 
-export default function Header() {
+export default function Header(props:any) {
     return (
         <header className="header">
             <Link to="/"><div className="home-container">
@@ -12,6 +13,10 @@ export default function Header() {
             </div></Link>
             <div className="logo-container">
                 <p className="logo"> Web Trainer</p>
+            </div>
+
+            <div onClick={props.onOpenSidebar} className={props.isSidebarOpen? "menu-open"  : "home-container"}>
+                <img  className="menu" src={menu} alt="Меню" />
             </div>
         </header >
     )
